cdd1b3a31d07302c4241b9885cd334673051b91e
Aamir-pathan-ekcs <maakhan@ekcs.co>
2024-12-10 18:14:23 +0530

    first banner second

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// Serve Chart.js from 'node_modules'
+// Serve Chart.js from 'node_modules';
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
